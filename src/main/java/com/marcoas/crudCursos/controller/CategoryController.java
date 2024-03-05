c9d2960359945157f9329af4f078b1d389303ff7
MarcoAs25 <marcoas2566@gmail.com>
2024-03-05 06:28:26 -0300

    impl controllers

---
+++ src/main/java/com/marcoas/crudCursos/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.marcoas.crudCursos.controller;
+
+import com.marcoas.crudCursos.dto.CategoryDTO;
+import com.marcoas.crudCursos.dto.PaginateDTO;
+import com.marcoas.crudCursos.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/category", produces = {"application/json"})
+public class CategoryController {
+    private final CategoryService service;
+
+    @GetMapping
+    private ResponseEntity<?> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/pageable")
+    private ResponseEntity<?> findAllPageable(@RequestParam(name = "size") Long size, @RequestParam(name = "page") Long page){
+        return ResponseEntity.ok(service.findAllPageable(new PaginateDTO(size, page)));
+    }
+
+    @GetMapping(value = "/{id}")
+    private ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    private ResponseEntity<?> create(@RequestBody CategoryDTO dto){
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @PutMapping(value = "/{id}")
+    private ResponseEntity<?> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    private ResponseEntity<?> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/marcoas/crudCursos/controller/CourseController.java
@@ -0,0 +1,44 @@
+package com.marcoas.crudCursos.controller;
+
+import com.marcoas.crudCursos.dto.CourseDTO;
+import com.marcoas.crudCursos.dto.PaginateDTO;
+import com.marcoas.crudCursos.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/course")
+public class CourseController {
+    private final CourseService service;
+
+    @GetMapping
+    private ResponseEntity<?> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/pageable")
+    private ResponseEntity<?> findAllPageable(@RequestParam(name = "size") Long size, @RequestParam(name = "page") Long page){
+        return ResponseEntity.ok(service.findAllPageable(new PaginateDTO(size, page)));
+    }
+    @GetMapping(value = "/{id}")
+    private ResponseEntity<?> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    private ResponseEntity<?> create(@RequestBody CourseDTO dto){
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @PutMapping(value = "/{id}")
+    private ResponseEntity<?> update(@PathVariable Long id, @RequestBody CourseDTO dto){
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    private ResponseEntity<?> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
