5b26acbdc0bb06f5a215fa7f7632315705738aa0
MarcoAs25 <marcoas2566@gmail.com>
2024-03-05 06:27:12 -0300

    add baseService

---
+++ src/main/java/com/marcoas/crudCursos/service/templates/BaseService.java
@@ -0,0 +1,15 @@
+package com.marcoas.crudCursos.service.templates;
+
+import com.marcoas.crudCursos.dto.PaginateDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface BaseService<T, DTO> {
+    T create(DTO dto);
+    T update(Long id, DTO dto);
+    T findById(Long id);
+    List<T> findAll();
+    Page<T> findAllPageable(PaginateDTO paginateSortDTO);
+    void delete(Long id);
+}
