aa9ad722e77578801b3c87079e0f21c66ab49d58
MarcoAs25 <marcoas2566@gmail.com>
2024-03-07 20:53:35 -0300

    feat: adicionando documentação nos controllers

--- src/main/java/com/marcoas/crudCursos/controller/CategoryController.java
+++ src/main/java/com/marcoas/crudCursos/controller/CategoryController.java
@@ -1,14 +1,25 @@
 package com.marcoas.crudCursos.controller;
 
+import com.marcoas.crudCursos.controller.exception.ErrorDetails;
 import com.marcoas.crudCursos.dto.CategoryDTO;
 import com.marcoas.crudCursos.dto.PaginateDTO;
+import com.marcoas.crudCursos.model.Category;
 import com.marcoas.crudCursos.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,33 +27,69 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
     private final CategoryService service;
 
+    @Operation(summary = "Obtém uma lista de todas as categorias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista encontrada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @GetMapping
-    private ResponseEntity<?> findAll(){
+    private ResponseEntity<List<Category>> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
 
+    @Operation(summary = "Obtém uma página de categorias através com base no tamanho(size) e posição da página(page)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Página encontrada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @GetMapping("/pageable")
-    private ResponseEntity<?> findAllPageable(@RequestParam(name = "size") Long size, @RequestParam(name = "page") Long page){
+    private ResponseEntity<Page<Category>> findAllPageable(@RequestParam(name = "size") Long size, @RequestParam(name = "page") Long page) {
         return ResponseEntity.ok(service.findAllPageable(new PaginateDTO(size, page)));
     }
 
+    @Operation(summary = "Obtém uma categoria através do id ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @GetMapping(value = "/{id}")
-    private ResponseEntity<?> findById(@PathVariable Long id){
+    private ResponseEntity<Category> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
+
+    @Operation(summary = "Cria uma nova categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Categoria criada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "Conflito ao criar categoria", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
-    private ResponseEntity<?> create(@Valid @RequestBody CategoryDTO dto){
+    private ResponseEntity<Category> create(@Valid @RequestBody CategoryDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
     }
 
+    @Operation(summary = "Atualizar uma categoria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria atualizada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "Conflito ao atualizar categoria", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @PutMapping(value = "/{id}")
-    private ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody CategoryDTO dto){
+    private ResponseEntity<Category> update(@PathVariable Long id, @Valid @RequestBody CategoryDTO dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
 
+    @Operation(summary = "Excluir uma categoria pelo Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Categoria excluída / inexistente no banco de dados"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "Conflito ao excluir categoria", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @DeleteMapping(value = "/{id}")
-    private ResponseEntity<?> delete(@PathVariable Long id){
+    private ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/marcoas/crudCursos/controller/CourseController.java
+++ src/main/java/com/marcoas/crudCursos/controller/CourseController.java
@@ -1,45 +1,92 @@
 package com.marcoas.crudCursos.controller;
 
+import com.marcoas.crudCursos.controller.exception.ErrorDetails;
 import com.marcoas.crudCursos.dto.CourseDTO;
 import com.marcoas.crudCursos.dto.PaginateDTO;
+import com.marcoas.crudCursos.model.Course;
 import com.marcoas.crudCursos.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/course")
+@RequestMapping(value = "/api/course", produces = {"application/json"})
 public class CourseController {
     private final CourseService service;
 
+    @Operation(summary = "Obtém uma lista de todos os cursos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista encontrada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @GetMapping
-    private ResponseEntity<?> findAll(){
+    private ResponseEntity<List<Course>> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
+
+    @Operation(summary = "Obtém uma página de cursos através com base no tamanho(size) e posição da página(page)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Página encontrada", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @GetMapping("/pageable")
-    private ResponseEntity<?> findAllPageable(@RequestParam(name = "size") Long size, @RequestParam(name = "page") Long page){
+    private ResponseEntity<Page<Course>> findAllPageable(@RequestParam(name = "size") Long size, @RequestParam(name = "page") Long page) {
         return ResponseEntity.ok(service.findAllPageable(new PaginateDTO(size, page)));
     }
+
+    @Operation(summary = "Obtém um curso através do id ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Curso encontrado", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Curso não encontrado", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @GetMapping(value = "/{id}")
-    private ResponseEntity<?> findById(@PathVariable Long id){
+    private ResponseEntity<Course> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
+    @Operation(summary = "Cria um novo curso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Curso criado", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "Conflito ao criar curso", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @PostMapping
-    private ResponseEntity<?> create( @Valid @RequestBody CourseDTO dto){
+    private ResponseEntity<Course> create(@Valid @RequestBody CourseDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
     }
 
+    @Operation(summary = "Atualizar um curso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Curso atualizado", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Curso não encontrado", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "Conflito ao atualizar curso", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @PutMapping(value = "/{id}")
-    private ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody CourseDTO dto){
+    private ResponseEntity<Course> update(@PathVariable Long id, @Valid @RequestBody CourseDTO dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
 
+    @Operation(summary = "Excluir um curso pelo Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Curso excluído / inexistente no banco de dados"),
+            @ApiResponse(responseCode = "400", description = "Requisição Inválida", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "409", description = "Conflito ao excluir curso", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @DeleteMapping(value = "/{id}")
-    private ResponseEntity<?> delete(@PathVariable Long id){
+    private ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
