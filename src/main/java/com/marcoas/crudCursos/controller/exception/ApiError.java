63257f4b402a64e9134ae9efc6ef6d9ea2c446be
MarcoAs25 <marcoas2566@gmail.com>
2024-03-07 05:45:46 -0300

    feat: modificando comportamento do apiError (agora o padrao é 400 e podemos adicionar statuscode)

--- src/main/java/com/marcoas/crudCursos/controller/exception/ApiError.java
+++ src/main/java/com/marcoas/crudCursos/controller/exception/ApiError.java
@@ -6,10 +6,17 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class ApiError extends ResponseStatusException {
     private String message;
+
     public ApiError(String message) {
-        super(HttpStatusCode.valueOf(500));
+        super(HttpStatusCode.valueOf(400));
         this.message = message;
     }
+
+    public ApiError(String message, HttpStatusCode statusCode) {
+        super(statusCode);
+        this.message = message;
+    }
+
     @Override
     public String getMessage() {
         return this.message;
